57f7ededff86252ea03f0d24a17a6eb4fc34d792
Marko20002 <102490651+Marko20002@users.noreply.github.com>
2023-02-01 01:36:45 +0100

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,45 @@
+
+#1
+# x = int(input())
+# for i in range (0,x):
+#     print(i)
+
+#2
+# x = int(input())
+# for i in range (0,x):
+#     denar = int (input())
+#     print (denar/60," evra")
+
+# #3
+# x = int(input())
+# for i in range (0,x):
+#     if i %2==0:
+#         print(i)
+
+# # #4
+# counter=0
+# counter_m=0
+# while(True):
+#     name=str(input("vnesete imininja na lica dokolku sakate da prekinete pisete NE"))
+#     x = int (input())
+#     if x >18:
+#        counter+=1
+#     else:
+#         counter_m+=1 
+#     if name == "NE":
+#         break
+# print("ima {} maloletni i ima {} polnoletni".format(counter,counter_m))
+#5
+# money=240
+# zbir=0
+# while(True):
+#     product=str(input("vnesete produkt"))
+#     price = int(input())
+#     zbir+=price
+#     if zbir>money:
+#         print("nemate dovolno pari")
+#     else:
+#         print("plativte {} i kusur vu e {}".format(zbir,money-zbir))
+#     if product == "NE":
+#         break
+
